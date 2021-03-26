1bcac9586f34ccfff0a683afa6117e6bf2d81c55
afan0918 <70462625+afan0918@users.noreply.github.com>
2021-03-26 17:36:14 +0800

    Add files via upload

---
+++ 核/src/main/java/smile/classification/Classifier.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2010-2020 Haifeng Li. All rights reserved.
+ *
+ * Smile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Smile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Smile.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package smile.classification;
+
+import java.io.Serializable;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+
+/**
+ * A classifier assigns an input object into one of a given number of categories.
+ * The input object is formally termed an instance, and the categories are
+ * termed classes. The instance is usually described by a vector of features,
+ * which together constitute a description of all known characteristics of the
+ * instance.
+ * <p>
+ * Classification normally refers to a supervised procedure, i.e. a procedure
+ * that produces an inferred function to predict the output value of new
+ * instances based on a training set of pairs consisting of an input object
+ * and a desired output value. The inferred function is called a classifier
+ * if the output is discrete or a regression function if the output is
+ * continuous.
+ * 
+ * @param <T> the type of input object
+ * 
+ * @author Haifeng Li
+ */
+public interface Classifier<T> extends ToIntFunction<T>, ToDoubleFunction<T>, Serializable {
+    /**
+     * Predicts the class label of an instance.
+     *
+     * @param x the instance to be classified.
+     * @return the predicted class label.
+     */
+    int predict(T x);
+
+    /**
+     * The classification score function.
+     *
+     * @param x the instance to be classified.
+     * @return the prediction score.
+     */
+    default double score(T x) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Predicts the class labels of an array of instances.
+     *
+     * @param x the instances to be classified.
+     * @return the predicted class labels.
+     */
+    default int[] predict(T[] x) {
+        int n = x.length;
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
+            y[i] = predict(x[i]);
+        }
+        return y;
+    }
+
+    /**
+     * 為了把weight拉出來，先創一個方法，之後再覆蓋掉
+     * @return weights of the model.
+     */
+    default double[] w(){
+        return new double[0];
+    }
+
+    /**
+     * 同上
+     * @return constant of the model.
+     */
+    default double b(){
+        return 0;
+    }
+
+    @Override
+    default int applyAsInt(T x) {
+        return predict(x);
+    }
+
+    @Override
+    default double applyAsDouble(T x) {
+        return score(x);
+    }
+}
