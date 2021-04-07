61e04e1ce4c7ef8d3d2123a822082d479d5bc50f
afan0918 <70462625+afan0918@users.noreply.github.com>
2021-04-07 18:30:52 +0800

    Delete main/src directory

--- main/src/DataBase.java
+++
@@ -1,6 +0,0 @@
-public class DataBase {
-    public static String DataPath = "Please import a file.";
-    public static double minAUC = 0.5;
-    public static CSVFile csvFile = new CSVFile();
-    public static ROCcurve ROC = new ROCcurve();
-}
